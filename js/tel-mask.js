cd1811faad34cf1879249dd81dee5f1db60cf13d
necoop <amok2005@gmail.com>
2022-12-16 04:25:09 +0700

    v 0.18 обои раздел О НАС

--- js/tel-mask.js
+++ js/tel-mask.js
@@ -5,7 +5,7 @@ window.addEventListener("DOMContentLoaded", function () {
       event.keyCode && (keyCode = event.keyCode);
       var pos = this.selectionStart;
       if (pos < 3) event.preventDefault();
-      var matrix = "+998 (__) ___ __ __",
+      var matrix = "+7 (___) ___-__-__",
         i = 0,
         def = matrix.replace(/\D/g, ""),
         val = this.value.replace(/\D/g, ""),
